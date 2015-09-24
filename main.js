bee897b6bf62661e4238bb24a90571dcba408c5c
Fabrício Rissetto <fabriciorissetto@gmail.com>
2015-09-24 20:05:27 -0300

    Inclusão de ícone de Tray

--- main.js
+++ main.js
@@ -2,53 +2,41 @@ var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 var Menu = require('menu');
 var Tray = require('tray');
-	
-// Report crashes to our server.
+var dialog = require('dialog');
 require('crash-reporter').start();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function () {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});  
-  
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+app.on('ready', function () {   
+    mainWindow = new BrowserWindow({ width: 500, height: 300 });    
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  mainWindow.openDevTools();
+    //mainWindow.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-  
-	var appIcon = null;
-	appIcon = new Tray('cwi.png');
-	  
-	var contextMenu = Menu.buildFromTemplate([
-		{ label: 'Item1', type: 'radio' },
-		{ label: 'Item2', type: 'radio' },
-		{ label: 'Item3', type: 'radio', checked: true },
-		{ label: 'Item4', type: 'radio' }
-	]);
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
 
-	appIcon.setToolTip('This is my application.');
-	appIcon.setContextMenu(contextMenu);
+    adicionaIconeDeTray();
 });
+
+function adicionaIconeDeTray() {
+    var appIcon = new Tray('cwi.png');
+
+    appIcon.on('clicked', function () {
+        //inserir chamada da janela aqui
+    });
+
+    var contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1' }
+    ]);
+
+    appIcon.setToolTip('CWI Software');
+    appIcon.setContextMenu(contextMenu);
+}
